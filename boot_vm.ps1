4a11ff2f9133551beb0a13611fa53ae3b38f9742
showgayaki <knk.lee.kj@gmail.com>
2021-01-30 15:55:22 +0900

    iniファイル読み込み変更

--- boot_vm.ps1
+++ boot_vm.ps1
@@ -1,32 +1,51 @@
-﻿function VirtualMachineName {
-    # iniファイルの最終行から、仮想マシン名を読み込み
-    $line = Get-Content ./boot_vm.ini -Tail 1;
-    $my_name = $line.Split("=")[1];
-    return $my_name;
+﻿function ReadIniFile {
+    param (
+        [parameter(mandatory=$true)][string]$file_name
+    )
+    $configure = @{};
+    $lines = Get-Content $file_name;
+    foreach($line in $lines){
+        # コメントと空行を除外する
+        if($line -match "^$"){ continue }
+        if($line -match "^\s*;"){ continue }
+
+        $param = $line.split("=", 2);
+        $configure.Add($param[0], $param[1]);
+      }
+    return $configure;
 }
 
 function Main {
+    $ini_file_name = "boot_vm.ini";
     # ウィンドウ非表示
     PowerShell -WindowStyle Hidden -Command Exit
-    $machine_name = VirtualMachineName;
-
     # .NET Frameworkのダイアログ関連オブジェクト
     Add-Type -AssemblyName System.Windows.Forms;
-    # 仮想マシンの存在チェック
-    if(ExistsVirtualMachine $machine_name){
-        # 起動してなかったらダイアログ表示して起動、起動実行したら起動確認
-        if(IsRunning $machine_name){
-            $contents = DialogContents "running" $machine_name;
-            $dialog = ShowDialog $contents;
-        }Else{
-            $contents = DialogContents "off" $machine_name;
-            $dialog = ShowDialog $contents;
-            if($dialog -eq "OK"){
-                BootVirtualMachine $machine_name $dialog
+
+    # iniファイル存在チェック
+    if(Test-Path $ini_file_name){
+        $config = ReadIniFile $ini_file_name;
+        $machine_name = $config["name"];
+
+        # 仮想マシンの存在チェック
+        if(ExistsVirtualMachine $machine_name){
+            # 起動してなかったらダイアログ表示して起動、起動実行したら起動確認
+            if(IsRunning $machine_name){
+                $contents = DialogContents "running" $machine_name;
+                $dialog = ShowDialog $contents;
+            }Else{
+                $contents = DialogContents "off" $machine_name;
+                $dialog = ShowDialog $contents;
+                if($dialog -eq "OK"){
+                    BootVirtualMachine $machine_name $dialog
+                }
             }
+        }Else{
+            $contents = DialogContents "not_exists" $machine_name;
+            $dialog = ShowDialog $contents;
         }
-    }Else{
-        $contents = DialogContents "not_exists" $machine_name;
+    }else {
+        $contents = DialogContents "no_ini_file" $ini_file_name;
         $dialog = ShowDialog $contents;
     }
 }
@@ -81,12 +100,20 @@ function ShowDialog {
 function DialogContents {
     param (
         [parameter(mandatory=$true)][string]$status,
-        [parameter(mandatory=$true)][string]$machine_name
+        [parameter(mandatory=$true)][string]$target
     )
     switch ($status) {
+        "no_ini_file" {
+            $contents = @{
+                "text" = "「{0}」が見つかりません。`r`nアプリケーションを終了します。" -f $target;
+                "caption" = "エラー";
+                "buttons_type" = "OK";
+                "icon_type" = "Warning";
+            }
+        }
         "not_exists" {
             $contents = @{
-                "text" = "「{0}」という仮想マシンは存在しません。" -f $machine_name;
+                "text" = "「{0}」という仮想マシンは存在しません。" -f $target;
                 "caption" = "エラー";
                 "buttons_type" = "OK";
                 "icon_type" = "Warning";
@@ -94,7 +121,7 @@ function DialogContents {
         }
         "off" {
             $contents = @{
-                "text" = "{0}を起動しますか？" -f $machine_name;
+                "text" = "{0}を起動しますか？" -f $target;
                 "caption" = "起動確認";
                 "buttons_type" = "OKCancel";
                 "icon_type" = "Question";
@@ -102,7 +129,7 @@ function DialogContents {
         }
         "success" {
             $contents = @{
-                "text" = "{0}の起動に成功しました。" -f $machine_name;
+                "text" = "{0}の起動に成功しました。" -f $target;
                 "caption" = "成功";
                 "buttons_type" = "OK";
                 "icon_type" = "Information";
@@ -110,7 +137,7 @@ function DialogContents {
         }
         "running" {
             $contents = @{
-                "text" = "{0}はすでに起動しています。" -f $machine_name;
+                "text" = "{0}はすでに起動しています。" -f $target;
                 "caption" = "終了";
                 "buttons_type" = "OK";
                 "icon_type" = "Warning";
